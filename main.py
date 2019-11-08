11934de44f239762dc1c2594884cf4ad49051457
Wildwizard404 <53104415+Wildwizard404@users.noreply.github.com>
2019-11-09 00:19:05 +0800

    temp

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-# -- coding: UTF-8 --
-import re
-import urllib
+import qqzhpt
 import sys
-import urllib.request
-from PIL import Image
-def work(url):
-    request = urllib.request.Request(url)
-    response = urllib.request.urlopen(request)
-    page = response.read().decode('utf-8')
-    #print(page)
-    reg=r'src="(.+?\.jpg)"'     #正则表达是筛选图片格式
-    img = re.compile(reg)       #创建模式对象
-    imglist = re.findall(img,page)   #解析页面源码获取图片列表
-    for imgurl in imglist:
-        try:
-            name = imgurl.split("img/")[1]
-            print(name)
-            picget = urllib.request.urlopen(imgurl).read()
-            print(type(picget))
-            with open("temp", "wb") as code:
-                code.write(picget)
-            Image.open("temp").save(name)
-        except:
-            print('Unexpected error:',sys.exc_info())
-    return imglist
-
-if __name__=='__main__':
-    url = "http://www.qqzhpt.com/meitu/detail/1562014808513"
-    for i in range(1,3):
-        work(url+str(i))
+if __name__ == '__main__' :
+    if len(sys.argv) != 1:
+        url = sys.argv[1]
+    else:
+        url = input("请输入套图首页URL\n")
+    mission = qqzhpt.meitu(url)
+    mission.run()
---
+++ qqzhpt.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import requests
+import _thread
+import os,time
+class meitu(object):
+    def __init__(self,url):
+        self.url = url
+        self.urls = []
+        self.dir = url.split('/')[-1]+'/'
+        if os.path.exists(self.dir) == False:
+            os.mkdir(self.dir)
+        self.locks = []
+        self.nloops = []
+        self.direct = ''
+    def download(self,url,lock):
+        web_content = requests.get(url)
+        soup = BeautifulSoup(web_content.text,'lxml')
+        imgs_list = soup.body.find(class_ = "imgs-list").div.find_all("img")
+        imgs_urls = []
+        for i in imgs_list:
+            imgs_urls.append(i.attrs["src"])
+        print("解析图片地址成功，开始下载！")
+        self.direct = imgs_urls[0].split('/')[:-1][-1]
+        for u in imgs_urls:
+            r = requests.get(u)
+            with open(self.dir+u.split('/')[-1].split('?')[0], "wb") as code:
+                code.write(r.content)
+        lock.release()
+    def geturls(self):
+        web_content = requests.get(self.url)
+        soup = BeautifulSoup(web_content.text,'lxml')
+        detail_page = soup.body.find(class_ = ['detail-pag','meitu-page']).find_all("a")
+        for i in detail_page:
+            self.urls.append(i.attrs["href"])
+        print("解析页面地址成功！")
+    def check(self):
+        files = os.listdir(self.dir)
+        fails = []
+        for i in files:
+            if os.path.getsize(self.dir+i) == 0:
+                fails.append(i)
+        if fails != []:
+            self.direct = "http://img.qqzhpt.com/meitustatic/images/img/"+self.direct+'/'
+            print("存在文件下载失败，尝试重新下载")
+            for f in fails:
+                r = requests.get(self.direct+f)
+                with open(self.dir+f, "wb") as code:
+                    code.write(r.content)
+    def run(self):
+        print("爬取开始！")
+        self.geturls()
+        self.nloops = range(len(self.urls))
+        for i in self.nloops:
+            lock = _thread.allocate_lock()
+            lock.acquire()
+            self.locks.append(lock)
+        for i in self.nloops:
+            _thread.start_new_thread(self.download,(self.urls[i],self.locks[i],))
+        while True:
+            flag = 0
+            for i in self.nloops:
+                if self.locks[i].locked():
+                    flag = 1
+            if flag == 0:
+                break
+        self.check()
